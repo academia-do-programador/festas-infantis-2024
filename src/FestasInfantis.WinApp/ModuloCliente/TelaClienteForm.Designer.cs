724171054cc7929e0b631a9f9106a6437aea82a2
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-06-10 09:53:21 -0300

    Criando formulário de cliente

---
+++ src/FestasInfantis.WinApp/ModuloCliente/TelaClienteForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace FestasInfantis.WinApp.ModuloCliente
+{
+    partial class TelaClienteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGravar = new Button();
+            btnCancelar = new Button();
+            label1 = new Label();
+            txtId = new TextBox();
+            label2 = new Label();
+            txtNome = new TextBox();
+            label3 = new Label();
+            txtTelefone = new MaskedTextBox();
+            lblCpf = new Label();
+            txtCpf = new MaskedTextBox();
+            SuspendLayout();
+            // 
+            // btnGravar
+            // 
+            btnGravar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGravar.DialogResult = DialogResult.OK;
+            btnGravar.Font = new Font("Segoe UI", 11.25F);
+            btnGravar.Location = new Point(299, 195);
+            btnGravar.Name = "btnGravar";
+            btnGravar.Size = new Size(90, 41);
+            btnGravar.TabIndex = 0;
+            btnGravar.Text = "Gravar";
+            btnGravar.UseVisualStyleBackColor = true;
+            btnGravar.Click += btnGravar_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancelar.DialogResult = DialogResult.Cancel;
+            btnCancelar.Font = new Font("Segoe UI", 11.25F);
+            btnCancelar.Location = new Point(395, 195);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(90, 41);
+            btnCancelar.TabIndex = 1;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F);
+            label1.Location = new Point(59, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Id:";
+            // 
+            // txtId
+            // 
+            txtId.Enabled = false;
+            txtId.Font = new Font("Segoe UI", 11.25F);
+            txtId.Location = new Point(90, 44);
+            txtId.Name = "txtId";
+            txtId.ReadOnly = true;
+            txtId.Size = new Size(100, 27);
+            txtId.TabIndex = 3;
+            txtId.Text = "0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F);
+            label2.Location = new Point(31, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            txtNome.Font = new Font("Segoe UI", 11.25F);
+            txtNome.Location = new Point(90, 79);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(395, 27);
+            txtNome.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F);
+            label3.Location = new Point(15, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 20);
+            label3.TabIndex = 10;
+            label3.Text = "Telefone:";
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Font = new Font("Segoe UI", 11.25F);
+            txtTelefone.Location = new Point(90, 112);
+            txtTelefone.Mask = "(00) 90000-0000";
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(157, 27);
+            txtTelefone.TabIndex = 11;
+            // 
+            // lblCpf
+            // 
+            lblCpf.AutoSize = true;
+            lblCpf.Font = new Font("Segoe UI", 11.25F);
+            lblCpf.Location = new Point(48, 147);
+            lblCpf.Name = "lblCpf";
+            lblCpf.Size = new Size(36, 20);
+            lblCpf.TabIndex = 10;
+            lblCpf.Text = "CPF:";
+            // 
+            // txtCpf
+            // 
+            txtCpf.Culture = new System.Globalization.CultureInfo("");
+            txtCpf.Font = new Font("Segoe UI", 11.25F);
+            txtCpf.Location = new Point(90, 145);
+            txtCpf.Mask = "000.000.000-00";
+            txtCpf.Name = "txtCpf";
+            txtCpf.Size = new Size(157, 27);
+            txtCpf.TabIndex = 11;
+            // 
+            // TelaClienteForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(497, 248);
+            Controls.Add(txtCpf);
+            Controls.Add(lblCpf);
+            Controls.Add(txtTelefone);
+            Controls.Add(label3);
+            Controls.Add(txtNome);
+            Controls.Add(label2);
+            Controls.Add(txtId);
+            Controls.Add(label1);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnGravar);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaClienteForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Cadastro de Clientes";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnGravar;
+        private Button btnCancelar;
+        private Label label1;
+        private TextBox txtId;
+        private Label label2;
+        private TextBox txtNome;
+        private Label label3;
+        private MaskedTextBox txtTelefone;
+        private Label lblCpf;
+        private MaskedTextBox txtCpf;
+    }
+}
